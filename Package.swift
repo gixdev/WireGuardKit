01e505da216183a0bae774f5360f032a5f2bff1a
James <james@apple.com>
2025-06-25 11:26:35 +0800

    Add WireGuardKit Swift package with wg-go.xcframework

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "WireGuard-iOS",
+    platforms: [
+        .iOS(.v14),
+        .macOS(.v11)
+    ],
+    products: [
+        .library(
+            name: "WireGuardKit",
+            targets: ["WireGuardKit"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "wg-go",
+            path: "./Libs/wg-go.xcframework"
+        ),
+        .target(
+          name: "WireGuardKitC",
+          dependencies: ["wg-go"],
+          publicHeadersPath: "include",
+          cSettings: [
+                 .headerSearchPath("include")
+             ]
+        ),
+        .target(
+            name: "WireGuardKit",
+            dependencies: [
+                "WireGuardKitC"
+            ]
+        )
+    ]
+)
